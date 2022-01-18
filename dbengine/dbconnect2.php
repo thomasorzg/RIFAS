fd40ec674581bc9f75c156a83f26d4176686a5b4
Thomas <jaapagamerz12@gmail.com>
2022-01-18 15:53:58 -0700

    Delete dbconnect2.php

--- dbengine/dbconnect2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','rifasma1_root','malverde0124','rifasma1_booking_db');
-if(!$conn) {
-	echo "database failed to open...";
-}
-mysqli_set_charset($conn, 'utf8');
-?>
