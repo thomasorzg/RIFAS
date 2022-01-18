388afc0b1124b5a48a85b0c2b5880478bc6d5518
Thomas <jaapagamerz12@gmail.com>
2022-01-18 16:08:12 -0700

    Update connection

--- dbengine/dbconnect.php
+++ dbengine/dbconnect.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
@@ -7,15 +8,15 @@ $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
+*/
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-mysqli_set_charset($conn, "utf8");
+$conn = mysqli_connect("us-cdbr-east-05.cleardb.net", "bd5451d44bcc7d", "180c444f", "heroku_caebebeebb91053");
 
 if(!$conn) {
 	echo "Database failed to open!";
-} /*else {
-	echo "Database connected successfully...";
-}*/
+}
+
+mysqli_set_charset($conn, "utf8");
 
 ?>
