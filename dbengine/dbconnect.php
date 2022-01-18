77921216ec927153c4e2df8a7094b54fd1ad8ca4
Thomas <jaapagamerz12@gmail.com>
2022-01-18 15:54:22 -0700

    Connection with ClearDB

--- dbengine/dbconnect.php
+++ dbengine/dbconnect.php
@@ -1,8 +1,21 @@
 <?php
+// Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-$conn = mysqli_connect('localhost','root','tommyo24','boletos');
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+mysqli_set_charset($conn, "utf8");
+
 if(!$conn) {
-	echo "database failed to open...";
+	echo "Database failed to open!";
+} else {
+	echo "Database connected successfully...";
 }
-mysqli_set_charset($conn, 'utf8');
+
 ?>
